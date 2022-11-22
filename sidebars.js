ac21f5e55ee422a1c37f20f7948f822de6fdd0a0
mxuan1998 <mxuan1990@gmail.com>
2022-11-22 15:42:56 +0000

    add cart.com doc

--- sidebars.js
+++ sidebars.js
@@ -38,8 +38,9 @@ const sidebars = {
           label: 'Sources',
           items: [
             'setup-guide/sources/amazon-ads', 
-            'setup-guide/sources/amazon-selling-partner', 
+            'setup-guide/sources/amazon-seller-partner', 
             'setup-guide/sources/bigcommerce',
+            'setup-guide/sources/cart.com',
             'setup-guide/sources/magento',
             'setup-guide/sources/mysql',
             'setup-guide/sources/netsuite',
